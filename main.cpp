b46ee7a45f5339feb4882c06ed10edc578b06773
bllgg <gunathilakebllg@gmail.com>
2022-07-11 00:23:29 +0530

    Main function updated

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "ControllableTimer.h"
+#include <unistd.h>
+
+void sampleFunction() {
+    std::cout << "Hey.. After each 2s..." << std::endl;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ControllableTimer controllableTimer = ControllableTimer();
+
+    controllableTimer.set_interval(2);
+
+    controllableTimer.start(&sampleFunction);
+
+    sleep(30);
+
+    controllableTimer.set_interval(4);
+
+    sleep(30);
+
+    controllableTimer.stop();
+
     return 0;
 }
